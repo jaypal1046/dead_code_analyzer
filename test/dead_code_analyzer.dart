86eb5e117c62bcb95a9bc255ac27f325c8eee3d9
Jayprakash Pal <jaypal1046@gmail.com>
2025-05-24 00:47:21 +0530

    -- Fix the class comment not found and added test case for class handling

--- test/dead_code_analyzer.dart
+++ test/dead_code_analyzer.dart
@@ -1,91 +1,21 @@
-// File: lib/test_functions.dart
-// This file contains various function types to test the analyzer
+import 'dart:io';
+import 'class_analyzer/class_analyzer.dart';
+import 'function_analyzer/function_analyzer.dart';
 
-// Top-level function that is used
-void usedTopLevelFunction() {
-  print('This function is used');
-}
-
-// Top-level function that is unused (dead code)
-void unusedTopLevelFunction() {
-  print('This function is never called');
-}
-
-// Class with various method types
-abstract class TestBaseClass {
-  // Abstract method
-  void abstractMethod();
-
-  // Regular method that will be overridden
-  void overridableMethod() {
-    print('Base implementation');
-  }
-}
-
-class TestClass extends TestBaseClass {
-  // Static method that is used
-  static void usedStaticMethod() {
-    print('This static method is used');
+void main(List<String> args) {
+  if (args.isEmpty) {
+    print("Please specify a test to run: class or function");
+    exit(1);
   }
 
-  // Static method that is never called (dead code)
-  static void unusedStaticMethod() {
-    print('This static method is never called');
+  switch (args[0]) {
+    case 'class':
+      classAnalyzerTest();
+      break;
+    case 'function':
+      functionAnalyzerTest();
+      break;
+    default:
+      print("Unknown test: ${args[0]}");
   }
-
-  // Instance method that is used
-  void usedInstanceMethod() {
-    print('This instance method is used');
-  }
-
-  // Instance method that is never called (dead code)
-  void unusedInstanceMethod() {
-    print('This instance method is never called');
-  }
-
-  // Implementation of abstract method
-  @override
-  void abstractMethod() {
-    print('Implementation of abstract method');
-  }
-
-  // Override of parent method
-  @override
-  void overridableMethod() {
-    print('Overridden implementation');
-  }
-}
-
-void main() {
-  // Call the used top-level function
-  usedTopLevelFunction();
-
-  // Create an instance of TestClass
-  final testInstance = TestClass();
-
-  // Call the used instance method
-  testInstance.usedInstanceMethod();
-
-  // Call the used static method
-  TestClass.usedStaticMethod();
-
-  // Call the abstract method implementation
-  testInstance.abstractMethod();
-
-  // Call the overridden method
-  testInstance.overridableMethod();
-
-  // Use an anonymous function (closure)
-  closure() {
-    print('This is an anonymous function');
-  }
-
-  closure();
-
-  // Use a local function
-  void localFunction() {
-    print('This is a local function');
-  }
-
-  localFunction();
 }
